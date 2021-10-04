f95663ea2156a1897a142c109d9defd2fe668491
YanZinchenko <Zinchenko.yan@gmail.com>
2021-10-04 14:35:58 +0300

    creat Js cod Hover

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#1976D2', '#FFFF00', '#18FFFF', '#FF4081', '#00C853']
+const SQUERES_NUMBER = 750
+
+for (let i = 0; i < SQUERES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor (element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
